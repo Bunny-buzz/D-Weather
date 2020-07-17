db03c604d7f3a44fb0cae857c957abc7e00fda42
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-07-17 16:51:05 -0600

    Added Image forecast and made some edits

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-let now = new Date();
+let now = new Date(timestamp);
 let h3 = document.querySelector("h3");
-let date = now.getDate();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
 
 let days = [
   "Sunday",
@@ -20,27 +11,72 @@ let days = [
   "Saturday",
 ];
 let day = days[now.getDay()];
-h3.innerHTML = `${day} ${date}, ${hours}: ${minutes}`;
-
-//Feature 1
+h3.innerHTML = `${day} ${formatHours(timestamp)}`;
+//
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+//
 function showPosition(response) {
-  // console.log(response);
+  console.log(response.data);
   let city = document.querySelector("h1");
   document.querySelector("h2").innerHTML = `${Math.round(
     response.data.main.temp
   )}°C`;
+  //
+  let iconElement = document.querySelector("#icon");
+  //
   city.innerHTML = `${response.data.name}`;
   document.querySelector("#hum").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  //
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
+//
 
 function search(city) {
   let apiKey = "d431d2ed9ff419b2288a607b5abcf652";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showPosition);
+
+  //
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+
+  forecastElement.innerHTML = `       
+   <div class="col-2">
+          <h4>
+            ${formatHours(forecast.dt * 1000)}
+          </h4>
+          <img src="http://openweathermap.org/img/wn/${
+            forecast.weather[0].icon
+          }@2x.png"/>
+          <div class="forecast-temperature>"><strong>${Math.round(
+            forecast.main.temp_max
+          )}°</strong>${Math.round(forecast.main.temp_min)}°</div>
+        </div>`;
+  forecast = response.data.list[0];
+}
+//
 function handleSearch(event) {
   event.preventDefault();
   let city = document.querySelector("#search-text-input").value;
