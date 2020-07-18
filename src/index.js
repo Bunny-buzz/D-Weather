4f6e8df12fee898025c830c058bfcc9502f803d5
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-07-18 01:52:53 -0600

    Fixing JS

--- src/index.js
+++ src/index.js
@@ -74,7 +74,21 @@ function displayForecast(response) {
             forecast.main.temp_max
           )}°</strong>${Math.round(forecast.main.temp_min)}°</div>
         </div>`;
-  forecast = response.data.list[0];
+  forecast = response.data.list[1];
+  forecastElement.innerHTML =
+    forecastElement.innerHTML +
+    `       
+   <div class="col-2">
+          <h4>
+            ${formatHours(forecast.dt * 1000)}
+          </h4>
+          <img src="http://openweathermap.org/img/wn/${
+            forecast.weather[0].icon
+          }@2x.png"/>
+          <div class="forecast-temperature>"><strong>${Math.round(
+            forecast.main.temp_max
+          )}°</strong>${Math.round(forecast.main.temp_min)}°</div>
+        </div>`;
 }
 //
 function handleSearch(event) {
